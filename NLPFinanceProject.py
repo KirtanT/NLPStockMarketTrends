1b18613c9769842a564a30d69651deeb488568fd
Kirtan Thakkar <72053016+KirtanT@users.noreply.github.com>
2024-04-01 17:15:21 -0400

    Add files via upload

---
+++ NLPFinanceProject.py
@@ -0,0 +1,394 @@
+# -*- coding: utf-8 -*-
+
+
+!pip install transformers
+import os
+import gdown
+import torch
+import numpy as np
+import transformers
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.sequence import pad_sequences
+
+from transformers import BertTokenizer
+from transformers import get_linear_schedule_with_warmup
+from transformers import BertForSequenceClassification, AdamW, BertConfig
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+
+# % matplotlib inline
+
+# gdown.download('https://drive.google.com/uc?id=1q4U2gVY9tWEPdT6W-pdQpKmo152QqWLE', 'finance_train.csv', True)
+# gdown.download('https://drive.google.com/uc?id=1nIBqAsItwVEGVayYTgvybz7HeK0asom0', 'finance_test.csv', True)
+
+!wget 'https://storage.googleapis.com/inspirit-ai-data-bucket-1/Data/AI%20Scholars/Sessions%206%20-%2010%20(Projects)/Project%20-%20NLP%2BFinance/finance_test.csv'
+!wget 'https://storage.googleapis.com/inspirit-ai-data-bucket-1/Data/AI%20Scholars/Sessions%206%20-%2010%20(Projects)/Project%20-%20NLP%2BFinance/finance_train.csv'
+
+def get_finance_train():
+  df_train = pd.read_csv("finance_train.csv")
+  return df_train
+def get_finance_test():
+  df_test = pd.read_csv("finance_test.csv")
+  return df_test
+
+def flat_accuracy(preds, labels):
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return np.sum(pred_flat == labels_flat) / len(labels_flat)
+
+print ("Train and Test Files Loaded as train.csv and test.csv")
+
+LABEL_MAP = {0 : "negative", 1 : "neutral", 2 : "positive"}
+NONE = 4 * [None]
+RND_SEED=2020
+
+
+
+from IPython.display import HTML
+
+
+
+
+df_train = get_finance_train() 
+df_test = get_finance_test()
+
+
+
+sentences = df_train["Sentence"].values
+labels = df_train["Label"].values
+
+
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased", do_lower_case = True)
+
+
+
+print(tokenizer.vocab_size)
+
+
+print(sentences[0])
+TOKENIZED_SENTENCE = tokenizer.tokenize(sentences[0])
+
+
+
+tokenizer.convert_tokens_to_ids(TOKENIZED_SENTENCE)
+
+
+sentences_with_special_tokens = []
+for i in sentences:
+  sentences_with_special_tokens.append("[CLS] " + i + " [SEP]")
+
+
+tokenized_texts = []
+### YOUR CODE HERE
+for i in sentences_with_special_tokens:
+  tokenized_texts.append(tokenizer.tokenize(i))
+### END CODE
+
+
+input_ids = []
+### YOUR CODE HERE
+for i in tokenized_texts:
+    input_ids.append(tokenizer.convert_tokens_to_ids(i))
+### END CODE
+
+max_len = 0
+len_index = 0
+
+for i in range(len(input_ids)):
+  if len(input_ids[i]) > max_len:
+      max_len = len(input_ids[i])
+      len_index = i
+
+print(max_len)
+print(tokenized_texts[len_index])
+
+
+
+input_ids = pad_sequences(input_ids,
+                          maxlen=128, ### YOUR CODE HERE
+                          dtype="long",
+                          truncating="post",
+                          padding="post")
+
+print(input_ids[len_index])
+
+
+attention_masks = []
+
+###YOUR CODE HERE###
+mask=[float(i>0) for i in input_ids[0]]
+
+for j in input_ids:
+  mask=[float(i>0) for i in j]
+  attention_masks.append(mask)
+print(attention_masks[len_index])
+
+###END CODE ###
+
+
+
+X_train, X_val, y_train, y_val = train_test_split(input_ids, labels, test_size = 0.15, random_state = RND_SEED)
+
+
+train_masks, validation_masks, _, _ = train_test_split(attention_masks, input_ids, test_size = 0.15, random_state=RND_SEED
+
+
+#@title Run this cell to convert data to tensors and create DataLoaders
+train_inputs = torch.tensor(np.array(X_train));
+validation_inputs = torch.tensor(np.array(X_val));
+train_masks = torch.tensor(np.array(train_masks));
+validation_masks = torch.tensor(np.array(validation_masks));
+train_labels = torch.tensor(np.array(y_train));
+validation_labels = torch.tensor(np.array(y_val));
+
+batch_size = 32
+train_data = TensorDataset(train_inputs, train_masks, train_labels);
+train_sampler = RandomSampler(train_data); # Samples data randonly for training
+train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size);
+validation_data = TensorDataset(validation_inputs, validation_masks, validation_labels);
+validation_sampler = SequentialSampler(validation_data); # Samples data sequentially
+validation_dataloader = DataLoader(validation_data, sampler=validation_sampler, batch_size=batch_size);
+
+
+
+
+model = BertForSequenceClassification.from_pretrained(
+    "bert-base-uncased", # Use the 12-layer BERT small model, with an uncased vocab.
+    num_labels = 3,
+    output_attentions = False, # Whether the model returns attentions weights.
+    output_hidden_states = False, # Whether the model returns all hidden-states.
+);
+
+
+model.cuda();
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+n_gpu = torch.cuda.device_count()
+torch.cuda.get_device_name(0)
+
+
+
+optimizer = AdamW(model.parameters(),
+                  lr = 2e-5, ### YOUR CODE HERE
+                  eps = 1e-8
+                )
+epochs = 4
+
+
+
+#@title Exercise. Ordered Model Training Steps
+
+_1_ = 'Unpack Data' #@param {type:"string"}
+_2_ = 'Forward Pass' #@param {type:"string"}
+_3_ = 'Update parameters' #@param {type:"string"}
+_4_ = 'Backwards pass' #@param {type:"string"}
+_5_ = 'forward pass' #@param {type:"string"}
+
+print('{}\n{}\n{}\n{}\n{}'.format(
+    '1. Unpack our data inputs and labels from the DataLoader objects',
+    '2. Clear out the gradients calculated in the previous pass',
+    '3. Forward pass',
+    '4. Backward pass',
+    '5. Update hyperparamters'))
+
+
+total_steps = len(train_dataloader) * epochs
+
+scheduler = get_linear_schedule_with_warmup(optimizer,
+                                            num_warmup_steps = 0, # Default value in run_glue.py
+                                            num_training_steps = total_steps)
+
+
+training_loss = []
+validation_loss = []
+training_stats = []
+for epoch_i in range(0, epochs):
+    # Training
+    print('Epoch {:} / {:} ========'.format(epoch_i + 1, epochs))
+    print('Training the model')
+    # Reset the total loss for  epoch.
+    total_train_loss = 0
+    # Put the model into training mode.
+    model.train()
+    # For each batch of training data
+    for step, batch in enumerate(train_dataloader):
+        # Progress update every 40 batches.
+        if step % 20 == 0 and not step == 0:
+            # Report progress.
+            print('  Batch {:>5,}  of  {:>5,}. '.format(step, len(train_dataloader)))
+
+
+        b_input_ids = batch[0].to(device)
+        b_input_mask = batch[1].to(device)
+        b_labels = batch[2].to(device)
+
+
+        model.zero_grad()
+
+
+        outputs = model(b_input_ids,
+                             token_type_ids=None,
+                             attention_mask=b_input_mask,
+                             labels=b_labels)
+        loss = outputs[0]
+        logits = outputs[1]
+
+        total_train_loss += loss.item()
+
+
+        loss.backward()
+
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+
+        optimizer.step()
+        scheduler.step()
+
+    # Calculate the average loss over all of the batches.
+    avg_train_loss = total_train_loss / len(train_dataloader)
+
+    print("  Average training loss: {0:.2f}".format(avg_train_loss))
+
+
+    print("Evaluating on Validation Set")
+    model.eval()
+    total_eval_accuracy = 0
+    total_eval_loss = 0
+    nb_eval_steps = 0
+    # Evaluate data for one epoch
+    for batch in validation_dataloader:
+
+
+        b_input_ids = batch[0].to(device)
+        b_input_mask = batch[1].to(device)
+        b_labels = batch[2].to(device)
+
+
+        with torch.no_grad():
+
+            outputs = model(b_input_ids,
+                                   token_type_ids=None,
+                                   attention_mask=b_input_mask,
+                                   labels=b_labels)
+            loss = outputs[0]
+            logits = outputs[1]
+
+        total_eval_loss += loss.item()
+
+        logits = logits.detach().cpu().numpy()
+        label_ids = b_labels.to('cpu').numpy()
+
+
+        total_eval_accuracy += flat_accuracy(logits, label_ids)
+
+
+    avg_val_accuracy = total_eval_accuracy / len(validation_dataloader)
+    print("Validation Accuracy: {0:.2f}".format(avg_val_accuracy))
+
+    avg_val_loss = total_eval_loss / len(validation_dataloader)
+
+
+
+    print("Validation Loss: {0:.2f}".format(avg_val_loss))
+
+
+    training_loss.append(avg_train_loss)
+    validation_loss.append(avg_val_loss)
+
+    training_stats.append(
+        {
+            'epoch': epoch_i + 1,
+            'Training Loss': avg_train_loss,
+            'Valid. Loss': avg_val_loss,
+            'Valid. Accur.': avg_val_accuracy
+
+        }
+    )
+
+print("Training complete!")
+
+
+
+fig = plt.figure(figsize=(12,6))
+plt.title('Loss over Time')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+
+plt.plot(training_loss, label = 'train')
+plt.plot(validation_loss, label = 'validation')
+
+plt.legend()
+plt.show()
+
+
+
+test_sentences = df_test["Sentence"].values
+test_labels = df_test["Label"].values
+
+
+test_input_ids, test_attention_masks = [], []
+
+test_sentences = ["[CLS] " + sentence + " [SEP]" for sentence in test_sentences]
+
+tokenized_test_sentences = [tokenizer.tokenize(sent) for sent in test_sentences]
+
+test_input_ids = [tokenizer.convert_tokens_to_ids(txt) for txt in tokenized_test_sentences]
+
+test_input_ids = pad_sequences(test_input_ids,
+                               maxlen=128,
+                               dtype="long",
+                               truncating="post",
+                               padding="post")
+
+for sequence in test_input_ids:
+  mask = [float(i>0) for i in sequence]
+  test_attention_masks.append(mask)
+
+
+#@title Run this cell to convert data to tensors and create DataLoaders
+batch_size = 32
+test_input_ids = torch.tensor(test_input_ids)
+test_attention_masks = torch.tensor(test_attention_masks)
+test_labels = torch.tensor(test_labels)
+prediction_data = TensorDataset(test_input_ids, test_attention_masks, test_labels)
+prediction_sampler = SequentialSampler(prediction_data)
+prediction_dataloader = DataLoader(prediction_data, sampler=prediction_sampler, batch_size=batch_size)
+
+
+#@title Evaluate Test Set Accuracy!
+# Prediction on test set
+
+print('Predicting labels for {:,} test sentences...'.format(len(test_input_ids)))
+
+# Put model in evaluation mode
+model.eval()
+
+# Tracking variables
+predictions , true_labels = [], []
+
+# Predict
+for batch in prediction_dataloader:
+  # Add batch to GPU
+  batch = tuple(t.to(device) for t in batch)
+
+  # Unpack the inputs from our dataloader
+  b_input_ids, b_input_mask, b_labels = batch
+
+  with torch.no_grad():
+      # Forward pass, calculate logit predictions
+      outputs = model(b_input_ids, token_type_ids=None,
+                      attention_mask=b_input_mask)
+
+  logits = outputs[0]
+
+  # Move logits and labels to CPU
+  logits = logits.detach().cpu().numpy()
+  label_ids = b_labels.to('cpu').numpy()
+
+  # Store predictions and true labels
+  predictions.append(logits)
+  true_labels.append(label_ids)
+
+
+print ('Test Accuracy: {:.2%}'.format(flat_accuracy(logits, label_ids)))
